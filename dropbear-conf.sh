a042a5868bf490aae5fe4708b44ccef1e52302a7
lordey <lord.dey68@gmail.com>
2018-03-05 01:21:14 +0700

    first commit

--- dropbear-conf.sh
+++ dropbear-conf.sh
@@ -32,12 +32,36 @@ DROPBEAR_RECEIVE_WINDOW=65536
 
 END
 
+# BANNER
+cat > /etc/issue.net <<-END
+
+============================
+* Term of Service :
+- No DDOS
+- No Hacking
+- No Carding
+- No Torrent
+- No Spaming
+- No Phising
+- and other illegal programs.
+- No login with Plink Tunnel.
+- No ReShare Account.
+- No login over 2 Bitvise.
+- No login over 1 Device.
+* Notes :
+If You don't follow the rules, then:
+- Disabled Account , No ReFund & Bawel.
+============================
+* Regards  :
+- Dey Agustian (fb.com/mr.azue).
+- Hidayat_Azah¦ (fb.com/azah1120)
+============================
+END
+
 # restart dropbear
 echo "/bin/false" >> /etc/shells
 echo "/usr/sbin/nologin" >> /etc/shells
 service ssh restart
 service dropbear restart
 
-# selesai
-
-
+# SELESAI
